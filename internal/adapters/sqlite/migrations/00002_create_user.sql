ef3eb5ae2a01b4df58556e52785fc30d8ed2a61c
Jairo Calcina <j.calcina.v@gmail.com>
2025-11-26 18:23:02 -0300

    feat(database): add user table migration

---
+++ internal/adapters/sqlite/migrations/00002_create_user.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS user (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    last_names TEXT NOT NULL,
+    email TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    status TEXT DEFAULT 'active',
+    birth_date DATE NOT NULL,
+    address TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    role_id TEXT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS user;
+-- +goose StatementEnd
