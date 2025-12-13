bbdca7d9176b9ec8c0b205538a5dc4280346a67b
Jairo Calcina <j.calcina.v@gmail.com>
2025-12-13 02:09:18 -0300

    feat(env): add configuration file to load env variables

---
+++ config/env.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	Port      string
+	DBUrl     string
+	JWTSecret string
+}
+
+func GetString(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
+
+func Load() Config {
+	return Config{
+		Port:      GetString("PORT", "8080"),
+		DBUrl:     GetString("DB_URL", "users.db"),
+		JWTSecret: GetString("JWT_SECRET", "secret"),
+	}
+}
