58015c0c9b45ddac9a7f54e0a18d184d13929929
Jairo Calcina <j.calcina.v@gmail.com>
2025-12-13 01:39:26 -0300

    feat(api): add functions to read and write json request and responses

---
+++ internal/json/json.go
@@ -0,0 +1,18 @@
+package json
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Write(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func Read(r *http.Request, data any) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	return decoder.Decode(data)
+}
